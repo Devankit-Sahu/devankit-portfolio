1a299fe9f04a9606351903a8f1996603719777e8
Devankit Sahu <dev738288@gmail.com>
2023-12-25 15:57:12 +0530

    improve hero section

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -5,7 +5,9 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import heroImg from "../assets/hero.jpeg";
+import heroImg from "../assets/hero2.png";
+import { motion } from "framer-motion";
+
 const Herosection = () => {
   return (
     <div className="relative  bg-effect py-[5rem] px-[5rem] lg:px-10" id="home">
@@ -13,13 +15,30 @@ const Herosection = () => {
         <div className="h-full grid grid-cols-1 gap-y-10 md:grid-cols-2">
           <div className=" flex items-start flex-col gap-y-5 justify-center">
             <p className="text-5xl md:text-4xl lg:text-5xl flex flex-col gap-y-2">
-              <span className="head">Hi, I m</span>
-              <span className="md:text-4xl lg:text-5xl font-[700] text-green-400 head1">
+              <motion.span
+                initial={{ translateX: -100, opacity: 0 }}
+                animate={{ translateX: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="head"
+              >
+                Hi, I'm
+              </motion.span>
+              <motion.span
+                initial={{ translateX: -100, opacity: 0 }}
+                animate={{ translateX: 0, opacity: 1 }}
+                transition={{ duration: 0.7, delay: 0.7 }}
+                className="md:text-4xl lg:text-5xl font-[700] text-green-400 head1"
+              >
                 Devankit Sahu
-              </span>
+              </motion.span>
             </p>
 
-            <p className=" text-[1.5rem] lg:text-3xl">
+            <motion.p
+              initial={{ translateX: -100, opacity: 0 }}
+              animate={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className=" text-[1.5rem] lg:text-3xl"
+            >
               I m a{" "}
               <span className="text-[1.6rem] lg:text-4xl font-[700] text-red-400">
                 <Typewriter
@@ -33,11 +52,18 @@ const Herosection = () => {
                   cursorBlinking={false}
                 />
               </span>
-            </p>
+            </motion.p>
             <p className="text-md my-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-              qui.
+              <motion.span
+                initial={{ opacity: 0, rotateY: 180 }}
+                animate={{ opacity: 1, rotateY: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+                qui.
+              </motion.span>
             </p>
+
             <Button
               sx={{
                 borderRadius: "25px",
@@ -55,11 +81,14 @@ const Herosection = () => {
           </div>
 
           <div className="flex justify-center order-first md:order-last">
-            <img
-              src={heroImg}
-              alt=""
-              className="w-[400px] h-[400px] md:w-[30vw] md:h-[60vh] object-cover rounded-full md:rounded-[25px]"
-            />
+            <div className="bg-overlay w-[400px] h-[400px] rounded-full overflow-hidden">
+              <img
+                src={heroImg}
+                alt=""
+                className="w-full h-full object-cover blur-0"
+                // className="w-[400px] h-[400px] md:w-[30vw] md:h-[60vh] object-cover rounded-full md:rounded-[25px]"
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import CircularProgress from "@mui/material/CircularProgress";
 
 const SkillsSection = () => {
   return (
-    <div className='h-screen py-[1rem] bg-red-400' id='skills'>
+    <div className='h-screen py-[1rem]' id='skills'>
         <h2>Skills I have</h2>
+        <div className="grid grid-cols-2 gap-x-10">
+          <div></div>
+          <div>
+            {/* <CircularProgress /> */}
+          </div>
+        </div>
     </div>
   )
 }
