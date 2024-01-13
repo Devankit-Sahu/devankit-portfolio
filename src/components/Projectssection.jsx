c915f69432f76bf8e2bba2bac112c3ec7e2f2a23
Devankit Sahu <dev738288@gmail.com>
2024-01-13 17:13:08 +0530

    create project section

--- src/components/Projectssection.jsx
+++ src/components/Projectssection.jsx
@@ -1,29 +1,53 @@
 import React from 'react'
+import { projectList } from "../constants/constants";
 
 const Projectssection = () => {
   return (
-    <div className="h-screen relative bg-black text-white" id="projects">
-      {/* <div
-        className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center opacity-[.7]"
-        style={{
-          backgroundImage: "url('/background-overlay.png')",
-        }}
-      ></div> */}
-      <div className="container mx-auto px-10 h-full 2xl:px-40">
+    <div className="h-screen bg-black text-white" id="projects">
+      <div className="container mx-auto px-10 h-full 2xl:px-40 flex flex-col justify-evenly">
         <div className="flex justify-center">
-          <h1 className="text-5xl pt-5 capitalize heading relative">
-            Projects I
-            <span className="text-6xl text-[blue] font-[800]"> Have</span>
-          </h1>
+          <h1 className="text-5xl capitalize heading relative">Projects</h1>
         </div>
-        <div className="card-wrapper">
-          <div className="card grid">
-
-          </div>
+        <div className="h-[calc(100%-180px)] cursor-pointer bg-[#4598e648] rounded-[50px]">
+          {projectList.map((project) => (
+            <div key={project.name} className="h-full grid grid-cols-2">
+              <img
+                src={project.image}
+                alt=""
+                className="h-full w-full object-cover"
+              />
+              <div className="flex flex-col justify-center px-10">
+                <h1 className="text-5xl font-[600] mb-5">{project.name}</h1>
+                <p className="text-xl font-[300] mb-5">{project.desc}</p>
+                <div>
+                  <h2 className="text-xl capitalize font-bold mb-5">
+                    Technologies used :
+                  </h2>
+                  <ul className="flex gap-3">
+                    {project.tech.map((t) => (
+                      <li key={t.name}>
+                        <div className="w-[40px] h-[40px]">
+                          <img
+                            src={t.image}
+                            alt=""
+                            className="w-full h-full object-cover"
+                          />
+                        </div>
+                        <h2>{t.name}</h2>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <button className="border-[1px] border-white rounded-[50px] p-4 mt-6 cursor-pointer">
+                  View Project
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projectssection
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -25,3 +25,89 @@ export const navLinks = [
     active: false,
   },
 ];
+
+
+export const skillsAndtoolsObj = [
+  {
+    name: "ReactJs",
+    image: "/icons8-react.svg",
+  },
+  {
+    name: "Javscript",
+    image: "/icons8-javascript.svg",
+  },
+  {
+    name: "NodeJs",
+    image: "/icons8-nodejs.svg",
+  },
+  {
+    name: "Html",
+    image: "/icons8-html-logo.svg",
+  },
+  {
+    name: "Css",
+    image: "/icons8-css-logo.svg",
+  },
+  {
+    name: "TailwindCss",
+    image: "/icons8-tailwindcss.svg",
+  },
+  {
+    name: "Redux",
+    image: "/icons8-redux.svg",
+  },
+  {
+    name: "MongoDb",
+    image: "/icons8-mongodb.svg",
+  },
+  {
+    name: "Python",
+    image: "/icons8-python.svg",
+  },
+  {
+    name: "Django",
+    image: "/icons8-django.svg",
+  },
+  {
+    name: "Vs Code",
+    image: "/icons8-vscode.svg",
+  },
+  {
+    name: "Git",
+    image: "/icons8-git.svg",
+  },
+  {
+    name: "Github",
+    image: "/icons8-github.svg",
+  },
+];
+
+export const projectList = [
+  {
+    name: "E-Commerce Project",
+    image: "/ecommerce.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Maxime, magnam?",
+    tech: [
+      {
+        image: "/icons8-javascript.svg",
+        name: "Javascript",
+      },
+      {
+        image: "/icons8-react.svg",
+        name: "ReactJs",
+      },
+      {
+        image: "/icons8-nodejs.svg",
+        name: "NodeJS",
+      },
+      {
+        image: "/icons8-react.svg",
+        name: "ExpressJs",
+      },
+      {
+        image: "/icons8-mongodb.svg",
+        name: "MongoDb",
+      },
+    ],
+  },
+];
